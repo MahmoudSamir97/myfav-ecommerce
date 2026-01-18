5d434709b931f86760ea1c09d1fddfd6ed9ca117
Mahmoud samir <mahmoudsamirsayed97@gmail.com>
2026-01-18 16:38:56 +0200

    feat: implement axios instance with request/response interceptors

---
+++ src/app/api/axios/instance.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+  timeout: 15000,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('accessToken');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    return Promise.reject(error);
+  }
+);
--- src/app/providers/AppProviders.tsx
+++ src/app/providers/AppProviders.tsx
@@ -1,24 +1,20 @@
 import type { ReactNode } from 'react';
 import { I18nextProvider } from 'react-i18next';
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import i18n from './i18n/instance';
 import { ThemeProvider } from '@/app/providers';
 import { Toaster } from '@/shared/ui';
+import { QueryProvider } from '@/app/providers/query-client/QueryProvider';
 
 interface Props {
   children: ReactNode;
 }
 
-// const queryClient = new QueryClient();
-
 export function AppProviders({ children }: Props) {
   return (
     <I18nextProvider i18n={i18n}>
-      <ThemeProvider defaultTheme="light">
-        {/* <QueryClientProvider client={queryClient}> */}
+      <ThemeProvider>
         <Toaster />
-        {children}
-        {/* </QueryClientProvider> */}
+        <QueryProvider>{children}</QueryProvider>
       </ThemeProvider>
     </I18nextProvider>
   );
