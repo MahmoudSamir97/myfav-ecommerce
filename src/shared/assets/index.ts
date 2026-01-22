79be831e6c5f1957c9c7e80e02f383570b4fc2c0
Mahmoud samir <mahmoudsamirsayed97@gmail.com>
2026-01-22 14:32:53 +0200

    style: add close icon and announcement

--- src/shared/assets/index.ts
+++ src/shared/assets/index.ts
@@ -1,2 +1,3 @@
 export { default as AvatarIcon } from '@/shared/assets/icons/avatar-icon.svg?react';
+export { default as AnnouncementIcon } from '@/shared/assets/icons/announcement-icon.svg?react';
 export { default as CloseIcon } from '@/shared/assets/icons/close-icon.svg?react';
