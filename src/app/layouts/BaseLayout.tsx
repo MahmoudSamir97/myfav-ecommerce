0dc448ad8fc6e8c623b1cf435979666239f1e36f
Mahmoud samir <mahmoudsamirsayed97@gmail.com>
2026-01-22 14:42:58 +0200

    feat: refactor base layout component and main

--- src/app/layouts/BaseLayout.tsx
+++ src/app/layouts/BaseLayout.tsx
@@ -1,16 +1,11 @@
 import { Footer } from '@/widgets/footer';
-import { Navbar } from '@/widgets/navbar';
-import { AnnouncementBar } from '@/widgets/navbar/ui/AnnouncementBar';
 import { Outlet } from 'react-router-dom';
+import { Header } from '@/widgets/header';
 
 export const BaseLayout = () => {
   return (
     <div className="flex min-h-screen flex-col bg-background">
-      <header>
-        <AnnouncementBar />
-        <Navbar />
-      </header>
-
+      <Header />
       <main className="grow">
         <Outlet />
       </main>
